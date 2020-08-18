254123862e02b747aa8c24d45b1130eb2f46f934
Yunjae Jeong <yunjae2295@naver.com>
2020-08-18 22:41:13 +0900

    modified name

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
+        <p>안녕하세요 헬로블리입니다:)</p>
       </header>
     </div>
   );
