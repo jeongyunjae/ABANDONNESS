308497dd429d03e63b639a4f9f417e3f93edddc6
Yunjae Jeong <yunjae2295@naver.com>
2020-08-21 03:16:32 +0900

    my commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express"); //express 모듈 가져오기
 const app = express(); //새로운 express() 앱 생성
+const { User } = require("./models/User");
 const bodyParser = require("body-parser");
-
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//application/json
+
+app.use(bodyParser.json());
+
 mongoose
   .connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -12,9 +19,6 @@ mongoose
   })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
@@ -25,7 +29,23 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("Hello World!!!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  console.log("안녕", req.body);
+  user.save((err, userInfo) => {
+    if (err)
+      return res.json({
+        success: false,
+        err,
+      });
+    else
+      return res.status(200).json({
+        success: true,
+      });
+  });
 });
 
 app.listen(PORT, () => {
