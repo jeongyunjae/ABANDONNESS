22d0a55ef81e6fb088fb9a83e11bddd5f45ff82b
Yunjae Jeong <yunjae2295@naver.com>
2020-08-22 17:47:40 +0900

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>ABANDONNESS</p>
+        <p>ABANDONNESS site</p>
       </header>
     </div>
   );
