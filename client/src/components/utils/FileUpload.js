7cdf4f21f1006ce53472f5b53c522fada82abea7
Yunjae Jeong <yunjae2295@naver.com>
2020-09-07 03:43:07 +0900

    adding image uploads

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { UploadOutlined } from "@ant-design/icons";
 import Dropzone from "react-dropzone";
 import Axios from "axios";
 
 function FileUpload() {
+  const [Images, setImages] = useState([]);
   const dropHandler = (files) => {
     let formData = new FormData(); //파일 전송할때 같이 전송해줘야하는것(파일정보)
 
     const config = {
-      header: { "content-type": "multipart/form-data" }, //컨텐츠 정보를 백엔드에 같이 보내줌
+      header: { "content-type": "multipart/form-data" },
     };
+    formData.append("file", files[0]);
     Axios.post("/api/gallary/image", formData, config).then((response) => {
       if (response.data.success) {
-        alert("파일저장 성공");
+        console.log(response.data);
+        setImages([...Images, response.data.filePath]);
       } else {
-        alert("파일저장 실패");
+        console.log(response.data);
       }
     });
   };
+  console.log(process.env.NODE_ENV);
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
       <Dropzone onDrop={dropHandler}>
@@ -41,6 +46,28 @@ function FileUpload() {
           </div>
         )}
       </Dropzone>
+
+      <div
+        style={{
+          display: "flex",
+          width: "350px",
+          height: "240px",
+          overflowX: "auto",
+        }}
+      >
+        {Images.map((image, index) => (
+          <div key={index}>
+            <img
+              style={{ minWidth: "300px", width: "300px", height: "240px" }}
+              src={
+                process.env.NODE_ENV === "development"
+                  ? `http://localhost:5000/${image}`
+                  : `http://abandonness.herokuapp.com/${image}`
+              }
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express"); //express 모듈 가져오기
 const app = express(); //새로운 express() 앱 생성
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -21,10 +22,9 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.use(cors());
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/gallary", require("./routes/gallary"));
+app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
@@ -32,6 +32,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
 }
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
